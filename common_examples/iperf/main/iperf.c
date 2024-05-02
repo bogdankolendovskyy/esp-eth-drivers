50eede6e5a46e90d76c0496b5dc1d7b745d403ce
Bogdan Kolendovskyy <bogdan.kolendovskyy@espressif.com>
2024-05-02 14:45:37 +0200

    Resolve misc issues

--- common_examples/iperf/main/iperf.c
+++ common_examples/iperf/main/iperf.c
@@ -6,29 +6,32 @@
 #include "iperf_cmd.h"
 #include "sdkconfig.h"
 
+esp_netif_ip_info_t ip_info = {
+    .ip = {.addr = ESP_IP4TOADDR(192, 168, 1, 1)},
+    .netmask = {.addr =  ESP_IP4TOADDR(255, 255, 255, 0)},
+    .gw = {.addr = ESP_IP4TOADDR(192, 168, 1, 1)}
+};
+
 static void start_dhcp_server_at_connection(void *esp_netif, esp_event_base_t base, int32_t event_id, void *data)
 {
-    printf("WE ARE HERE\n");
+    esp_netif_dhcpc_stop(esp_netif);
+    esp_netif_set_ip_info(esp_netif, &ip_info);
     esp_netif_dhcps_start(esp_netif);
 }
-
 
 void app_main(void)
 {
     uint8_t eth_port_cnt = 0;
     esp_eth_handle_t *eth_handles;
+    esp_netif_config_t cfg;
+    esp_netif_inherent_config_t eth_netif_cfg;
     esp_netif_init();
     esp_event_loop_create_default();
     ethernet_init_all(&eth_handles, &eth_port_cnt);
 
 #if CONFIG_EXAMPLE_ACT_AS_DHCP_SERVER
-    // Act as DHCP server
-    esp_netif_ip_info_t ip_info = {
-        .ip = {.addr = ESP_IP4TOADDR(192, 168, 1, 1)},
-        .netmask = {.addr =  ESP_IP4TOADDR(255, 255, 255, 0)},
-        .gw = {.addr = ESP_IP4TOADDR(192, 168, 1, 1)}
-    };
-    esp_netif_inherent_config_t eth_behav_cfg = {
+    // Config for ESP32 to act as DHCP server
+    eth_netif_cfg = (esp_netif_inherent_config_t) {
         .get_ip_event = IP_EVENT_ETH_GOT_IP,
         .lost_ip_event = 0,
         .flags = ESP_NETIF_DHCP_SERVER,
@@ -37,61 +40,36 @@ void app_main(void)
         .if_desc = "eth",
         .route_prio = 50
     };
-    esp_netif_config_t cfg = {
-        .base = &eth_behav_cfg,
-        .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
-    };
+#else
+    if (eth_port_cnt == 1) {
+        // Use default config when using one interface
+        eth_netif_cfg = *(ESP_NETIF_BASE_DEFAULT_ETH);
+    } else {
+        // Set behavioral config to support multiple interfaces
+        eth_netif_cfg = (esp_netif_inherent_config_t) ESP_NETIF_INHERENT_DEFAULT_ETH();
+    }
+#endif
     char if_key_str[10];
     char if_desc_str[10];
+    cfg = (esp_netif_config_t) {
+        .base = &eth_netif_cfg,
+        .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
+    };
     for (int i = 0; i < eth_port_cnt; i++) {
         sprintf(if_key_str, "ETH_%d", i);
         sprintf(if_desc_str, "eth%d", i);
-        eth_behav_cfg.if_key = if_key_str;
-        eth_behav_cfg.if_desc = if_desc_str;
-        eth_behav_cfg.route_prio -= i * 5;
+        eth_netif_cfg.if_key = if_key_str;
+        eth_netif_cfg.if_desc = if_desc_str;
+        eth_netif_cfg.route_prio -= i * 5;
         esp_netif_t *eth_netif = esp_netif_new(&cfg);
         ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[i])));
-        // Attach Ethernet driver to TCP/IP stack
+
+#if CONFIG_EXAMPLE_ACT_AS_DHCP_SERVER
         esp_event_handler_register(ETH_EVENT, ETHERNET_EVENT_CONNECTED, start_dhcp_server_at_connection, eth_netif);
-        esp_netif_dhcpc_stop(eth_netif);
-        esp_netif_set_ip_info(eth_netif, &ip_info);
+#endif
+
         esp_eth_start(eth_handles[i]);
     }
-#else
-    // Act as DHCP client, usual behaviour
-    char if_key_str[10];
-    char if_desc_str[10];
-    // Create instance(s) of esp-netif for Ethernet(s)
-    if (eth_port_cnt == 1) {
-        // Use ESP_NETIF_DEFAULT_ETH when just one Ethernet interface is used and you don't need to modify
-        // default esp-netif configuration parameters.
-        esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
-        esp_netif_t *eth_netif = esp_netif_new(&cfg);
-        // Attach Ethernet driver to TCP/IP stack
-        ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
-    } else {
-        // Use ESP_NETIF_INHERENT_DEFAULT_ETH when multiple Ethernet interfaces are used and so you need to modify
-        // esp-netif configuration parameters for each interface (name, priority, etc.).
-        esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_ETH();
-        esp_netif_config_t cfg_spi = {
-            .base = &esp_netif_config,
-            .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
-        };
-
-        for (int i = 0; i < eth_port_cnt; i++) {
-            sprintf(if_key_str, "ETH_%d", i);
-            sprintf(if_desc_str, "eth%d", i);
-            esp_netif_config.if_key = if_key_str;
-            esp_netif_config.if_desc = if_desc_str;
-            esp_netif_config.route_prio -= i * 5;
-            esp_netif_t *eth_netif = esp_netif_new(&cfg_spi);
-
-            // Attach Ethernet driver to TCP/IP stack
-            ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[i])));
-            esp_eth_start(eth_handles[i]);
-        }
-    }
-#endif
     esp_console_repl_t *repl = NULL;
     esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
     esp_console_dev_uart_config_t uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
--- common_examples/tcp_client/main/tcp_client.c
+++ common_examples/tcp_client/main/tcp_client.c
@@ -77,7 +77,7 @@ void app_main(void)
         }
     }
     // Wait until IP address is assigned to this device
-    xSemaphoreTake(xGotIpSemaphore, portMAX_DELAY);
+    xSemaphoreTake(x_got_ip_semaphore, portMAX_DELAY);
     ESP_LOGI(TAG, "TCP client is started, waiting for the server to accept a connection.");
     int client_fd;
     struct sockaddr_in server;
